9887f01b00c8ab563e571136a502a07414ade4f5
daniele <f.daniele81@tiscali.it>
2019-05-15 12:29:54 +0200

    ++

---
+++ app/src/main/java/com/example/wallpaper/ImageAdapter.java
@@ -0,0 +1,58 @@
+package com.example.wallpaper;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+
+
+    private Context context;
+
+    private Integer[] array_idSfondi = {R.raw.lloyd_thumbs, R.raw.android_wallpaper, R.raw.images};
+
+
+
+    public ImageAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return array_idSfondi.length;
+    }
+
+
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if(convertView == null){
+            imageView = new ImageView(context);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8,8,8,8);
+        }
+        else imageView = (ImageView) convertView;
+
+        imageView.setImageResource(array_idSfondi[position]);
+        return imageView;
+    }
+}
--- app/src/main/java/com/example/wallpaper/MainActivity.java
+++ app/src/main/java/com/example/wallpaper/MainActivity.java
@@ -32,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
         updateWallpaper();
 
+        gridView.setAdapter(new ImageAdapter(this));
+
     }
 
 
